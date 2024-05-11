6e6c22d3758f91f1245f6949d7f2455a1cc7ff0d
Johnny-Asmar <Johnny@beytek.co>
2024-05-11 19:42:18 +0300

    firebase integration and fetch sectors on sign up

--- app/src/main/java/com/example/mediaplayerapp/SignupActivity.java
+++ app/src/main/java/com/example/mediaplayerapp/SignupActivity.java
@@ -3,13 +3,19 @@ package com.example.mediaplayerapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 public class SignupActivity extends AppCompatActivity {
+    private Spinner sectorsSpinner;
+    private FirestoreHelper firestoreHelper;
     private EditText editTextEmail;
     private EditText editTextPassword;
     private DatabaseHelper databaseHelper;
@@ -20,14 +26,32 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         databaseHelper = new DatabaseHelper(this);
+        firestoreHelper = new FirestoreHelper();
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
+        sectorsSpinner = findViewById(R.id.sectorsSpinner);
         Button buttonRegister = findViewById(R.id.buttonRegister);
-
+        loadSectors();
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 postDataToSQLite();
+            }
+        });
+    }
+
+    private void loadSectors() {
+        firestoreHelper.fetchSectors(new FirestoreHelper.FirestoreCallback() {
+            @Override
+            public void onCallback(List<String> sectorNames) {
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(SignupActivity.this,
+                        android.R.layout.simple_spinner_dropdown_item, sectorNames);
+                sectorsSpinner.setAdapter(adapter);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Toast.makeText(SignupActivity.this, "Failed to load sectors: " + e.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
